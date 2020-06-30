9dc48b5d8c663a5902ab4f741c04c8c9d8e68f3b
Md Shohel <Shohel@Mds-MacBook-Pro.attlocal.net>
2020-06-30 09:28:05 -0500

    UIDEV-1234 : clean up CRA

--- src/App.js
+++ src/App.js
@@ -1,26 +1,22 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  printName = () => 'Sanfeer';
+  render() {
+    // const name = 'Sanfeer';
+    const loading = false;
+    const showName = true;
+    return (
+      <React.Fragment>
+        {loading ? (
+          <h1>Loading...</h1>
+        ) : (
+          <h1>Hello {showName && this.printName()}</h1>
+        )}
+      </React.Fragment>
+    );
+  }
 }
 
 export default App;
